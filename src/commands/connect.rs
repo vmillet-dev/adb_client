47fd2307c1eb040dca7bebf7438c7cee85039455
Valentin MILLET <valentin.millet39@gmail.com>
2024-04-19 12:32:57 +0200

    Add pair and connect command

---
+++ src/commands/connect.rs
@@ -0,0 +1,15 @@
+use crate::{models::AdbCommand,  AdbTcpConnection, Result, RustADBError};
+use std::net::Ipv4Addr;
+
+impl AdbTcpConnection {
+    /// Connect device over tcp with address and port
+    pub fn connect(&mut self, address: Ipv4Addr, port: u16) -> Result<()> {
+        let response = self.proxy_connection(AdbCommand::Connect(address, port), true)?;
+
+        match String::from_utf8(response).unwrap() {
+            s if s.starts_with("connected to") => Ok(()),
+            s if s.starts_with("failed to connect to") => Err(RustADBError::ADBDeviceNotPaired),
+            s => Err(RustADBError::ADBRequestFailed(s))
+        }
+    }
+}
